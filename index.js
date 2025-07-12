5927778db2a52166f8a44b8e2a84af71c37aad01
Edinamm Cita Amekuedee <edinamamekuedee4@gmail.com>
2025-07-12 22:44:27 +0000

    Update game with html, css and js content

--- index.js
+++ index.js
@@ -1,6 +1,44 @@
-
-let firstCard = 5;
-let secondCard = 9;
+let firstCard = 3;
+let secondCard = 11;
+let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
+let isAlive = true;
+let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el")
+//let sumEl = document.querySelector("#sum-el");
+let cardEl = document.querySelector("#card-el")
 
-console.log(sum);
+function startGame() {
+ renderGame()
+}  
+
+function renderGame() {
+  sumEl.textContent = "Sum: " + sum;
+  cardEl.textContent = "Card: "
+
+for (let i = 0; i < cards.length; i++){
+  cardEl.textContent += cards[i] + " "
+}
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+  messageEl.textContent = message;
+  console.log(message)
+}
+
+
+function newCard() {
+  let card = 7;
+  sum += card;
+  cards.push(card);
+  renderGame();
+}
